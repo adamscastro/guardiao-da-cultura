a15d12d4a631f63b29b60b356911771f00082546
Adams Castro <adams.acastro@gmail.com>
2025-04-09 22:30:19 -0300

    Atualizações das Relíquias Ancestrais

---
+++ Scripts/Relics/carapuca.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+func _ready():
+	$AnimationPlayer.play("efeito_carapuca")
+	$AnimationPlayer2.play("efeito_ilusao")
---
+++ Scripts/Relics/fio.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+func _ready():
+	$AnimationPlayer.play("efeito_tremer")
+	$AnimationPlayer2.play("efeito_fio")
+	
